24fbf8897b2c629927799d7d1346160650ad2051
Marc LAMY <marclamy.bzh@gmail.com>
2020-11-12 20:38:31 +0100

    Updated the build.gradle to include the Room library and added the Book entity

---
+++ app/src/main/java/net/imt/fmsbookstore/model/Book.kt
@@ -0,0 +1,13 @@
+package net.imt.fmsbookstore.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Book(
+    @PrimaryKey val isbn: String,
+    val title: String,
+    val price: Double,
+    val cover: String,
+    val synopsis: List<String>
+)
