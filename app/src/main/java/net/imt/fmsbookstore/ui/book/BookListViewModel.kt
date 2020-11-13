a43661d054c0ab9e3a1eb6b09c7fe3890005d2c6
Marc LAMY <marclamy.bzh@gmail.com>
2020-11-13 14:10:58 +0100

    Injection of repository in BookListViewModel

--- app/src/main/java/net/imt/fmsbookstore/ui/book/BookListViewModel.kt
+++ app/src/main/java/net/imt/fmsbookstore/ui/book/BookListViewModel.kt
@@ -1,10 +1,27 @@
 package net.imt.fmsbookstore.ui.book
 
+import androidx.hilt.Assisted
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dagger.hilt.android.HiltAndroidApp
 import net.imt.fmsbookstore.data.book.Book
 import net.imt.fmsbookstore.data.book.BookRepository
 
-class BookListViewModel (private val bookRepository: BookRepository) : ViewModel() {
+/*class BookListViewModel (private val bookRepository: BookRepository) : ViewModel() {
+    val bookList : LiveData<List<Book>> = bookRepository.getBookList()
+
+    companion object {
+        fun create(modelClass: Class<T>): ViewModel {
+            @Suppress("UNCHECKED_CAST")
+            return BookListViewModel(bookRepository)
+        }
+    }
+}*/
+class BookListViewModel @ViewModelInject constructor(
+    bookRepository: BookRepository
+) : ViewModel() {
     val bookList : LiveData<List<Book>> = bookRepository.getBookList()
 }
