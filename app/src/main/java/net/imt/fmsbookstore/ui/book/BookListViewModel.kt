f385f4d0124dfdb0983ab1c0f1aab85bc0e89ccc
Marc LAMY <marclamy.bzh@gmail.com>
2020-11-13 15:01:01 +0100

    BookListViewModel cleaned

--- app/src/main/java/net/imt/fmsbookstore/ui/book/BookListViewModel.kt
+++ app/src/main/java/net/imt/fmsbookstore/ui/book/BookListViewModel.kt
@@ -10,16 +10,6 @@ import dagger.hilt.android.HiltAndroidApp
 import net.imt.fmsbookstore.data.book.Book
 import net.imt.fmsbookstore.data.book.BookRepository
 
-/*class BookListViewModel (private val bookRepository: BookRepository) : ViewModel() {
-    val bookList : LiveData<List<Book>> = bookRepository.getBookList()
-
-    companion object {
-        fun create(modelClass: Class<T>): ViewModel {
-            @Suppress("UNCHECKED_CAST")
-            return BookListViewModel(bookRepository)
-        }
-    }
-}*/
 class BookListViewModel @ViewModelInject constructor(bookRepository: BookRepository) : ViewModel() {
     val bookList : LiveData<List<Book>> = bookRepository.getBookList()
 }
