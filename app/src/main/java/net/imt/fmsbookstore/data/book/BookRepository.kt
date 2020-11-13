ce8f040e2e1348d861c640ac23d5e71bfe4dc346
Marc LAMY <marclamy.bzh@gmail.com>
2020-11-13 10:53:44 +0100

    Renamed BookApi to BookService and added lazy loading of BookService singleton

--- app/src/main/java/net/imt/fmsbookstore/data/book/BookRepository.kt
+++ app/src/main/java/net/imt/fmsbookstore/data/book/BookRepository.kt
@@ -3,9 +3,9 @@ package net.imt.fmsbookstore.data.book
 import androidx.lifecycle.LiveData
 import java.util.concurrent.Executor
 
-class BookRepository (private val bookApi: BookApi, private val bookDao: BookDao, private val ioExecutor: Executor) {
+class BookRepository (private val bookService: BookService, private val bookDao: BookDao, private val ioExecutor: Executor) {
     fun getBookList(): LiveData<List<Book>> {
-        bookApi.getBooks(bookApi, {
+        bookService.getBooks(bookService, {
             books -> ioExecutor.execute { bookDao.insertAll(books) }
         }, { error ->
             // If there is an error, the user will have access to the locally stored books
