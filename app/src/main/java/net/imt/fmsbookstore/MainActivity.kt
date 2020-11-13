b2aba3453f5dc8ccb21cdf2a3cd0b8941c3b6977
Marc LAMY <marclamy.bzh@gmail.com>
2020-11-13 14:54:41 +0100

    MainActivity with fragments container

--- app/src/main/java/net/imt/fmsbookstore/MainActivity.kt
+++ app/src/main/java/net/imt/fmsbookstore/MainActivity.kt
@@ -2,12 +2,15 @@ package net.imt.fmsbookstore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import net.imt.fmsbookstore.R
-import net.imt.fmsbookstore.data.book.getBooks
+import net.imt.fmsbookstore.ui.book.BookListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.containerFrameLayout, BookListFragment())
+            .commit()
     }
 }
