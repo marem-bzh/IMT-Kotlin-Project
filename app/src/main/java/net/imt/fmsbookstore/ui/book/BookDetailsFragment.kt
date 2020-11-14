cbfb9940f24818562342cb36bd5ff9f1a87c445c
Marc LAMY <marclamy.bzh@gmail.com>
2020-11-14 20:00:44 +0100

    Inflated the BookDetailsFragment with title and cover from the repository

--- app/src/main/java/net/imt/fmsbookstore/ui/book/BookDetailsFragment.kt
+++ app/src/main/java/net/imt/fmsbookstore/ui/book/BookDetailsFragment.kt
@@ -6,17 +6,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.squareup.picasso.Picasso
 import net.imt.fmsbookstore.R
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
 class BookDetailsFragment : Fragment() {
     private val viewModel: BookDetailsViewModel by viewModel(state = { arguments as Bundle })
+    private lateinit var bookTitleTextView: TextView
+    private lateinit var bookCoverImageView: ImageView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // View
-        val view = inflater.inflate(R.layout.fragment_book_detail, container, false)
+        val view = inflater.inflate(R.layout.book_details, container, false)
+
+        bookTitleTextView = view.findViewById(R.id.bookTitle)
+        bookCoverImageView = view.findViewById(R.id.bookCover)
 
         return view
     }
@@ -25,7 +33,9 @@ class BookDetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.book.observe(viewLifecycleOwner) {
-            Timber.i("${it?.title ?: "Unknown book"}")
+            bookTitleTextView.text = it.title
+            Picasso.get().load(it.cover).into(bookCoverImageView)
+            // TODO Synopsis
         }
     }
 }
