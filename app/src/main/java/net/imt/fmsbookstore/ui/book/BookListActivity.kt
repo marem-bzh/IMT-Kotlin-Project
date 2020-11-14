6bc36d35224a3cb9e210b51ceb1d65f613477998
Marc LAMY <marclamy.bzh@gmail.com>
2020-11-14 16:26:31 +0100

    Renamed MainActivity to BookListActivity

---
+++ app/src/main/java/net/imt/fmsbookstore/ui/book/BookListActivity.kt
@@ -0,0 +1,26 @@
+package net.imt.fmsbookstore.ui.book
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.imt.fmsbookstore.R
+import net.imt.fmsbookstore.data.book.Book
+import net.imt.fmsbookstore.ui.book.BookListFragment
+
+class BookListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.book_list_host)
+
+        if(savedInstanceState == null){
+            val fragment = BookListFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.main_content, fragment)
+                .commit()
+        }
+    }
+
+    fun show(book: Book){
+        // Handle navigation here
+    }
+}
