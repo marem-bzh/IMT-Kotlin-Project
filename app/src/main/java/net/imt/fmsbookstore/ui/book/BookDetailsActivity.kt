70c1471c0561c993f3872e59caf334471c0f1ec7
Marc LAMY <marclamy.bzh@gmail.com>
2020-11-14 17:02:38 +0100

    Created the activity and fragment of the book details

---
+++ app/src/main/java/net/imt/fmsbookstore/ui/book/BookDetailsActivity.kt
@@ -0,0 +1,22 @@
+package net.imt.fmsbookstore.ui.book
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.imt.fmsbookstore.R
+import net.imt.fmsbookstore.data.book.Book
+import net.imt.fmsbookstore.ui.book.BookListFragment
+
+class BookDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.book_details_host)
+
+        if(savedInstanceState == null){
+            val fragment = BookDetailsFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.main_content, fragment)
+                .commit()
+        }
+    }
+}
