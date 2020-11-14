4ce78e9af4dc6f85dbebaa5864559929c8846559
Marc LAMY <marclamy.bzh@gmail.com>
2020-11-14 19:07:16 +0100

    Created the BookDetailsViewModel and integrated the repository with the BookDetailsFragment

--- app/src/main/java/net/imt/fmsbookstore/ui/book/BookDetailsFragment.kt
+++ app/src/main/java/net/imt/fmsbookstore/ui/book/BookDetailsFragment.kt
@@ -1,21 +1,31 @@
 package net.imt.fmsbookstore.ui.book
 
+import android.app.Application
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import net.imt.fmsbookstore.R
-import net.imt.fmsbookstore.data.book.Book
-import net.imt.fmsbookstore.ui.PositionedClickListener
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
 class BookDetailsFragment : Fragment() {
+    private val viewModel: BookDetailsViewModel by viewModel(state = { arguments as Bundle })
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // View
         val view = inflater.inflate(R.layout.fragment_book_detail, container, false)
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.book.observe(viewLifecycleOwner) {
+            Timber.i("${it?.title ?: "Unknown book"}")
+        }
     }
 }
