f0247d9998db43d4cbb8ddf538ade5ab5331b8d5
samylegalloudec <samy.legalloudec@gmail.com>
2020-11-22 13:34:19 +0100

    implémentation fragment et adapter

--- app/src/main/java/net/imt/fmsbookstore/ui/cart/CartAdapter.kt
+++ app/src/main/java/net/imt/fmsbookstore/ui/cart/CartAdapter.kt
@@ -1,4 +1,82 @@
 package net.imt.fmsbookstore.ui.cart
 
-class CartAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import net.imt.fmsbookstore.R
+import net.imt.fmsbookstore.data.book.BookRepository
+import net.imt.fmsbookstore.data.cart.CartElement
+import net.imt.fmsbookstore.ui.PositionedClickListener
+
+class CartAdapter(var cartElementList : List<CartElement>, private val listener : PositionedClickListener) : RecyclerView.Adapter<CartAdapter.CartHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartHolder {
+        val inflatedView = LayoutInflater.from(parent.context).inflate(R.layout.cart_item, parent, false)
+        return CartHolder(inflatedView, listener)
+    }
+
+    override fun getItemCount(): Int {
+        return cartElementList.size
+    }
+
+    override fun onBindViewHolder(holder: CartHolder, position: Int) {
+        val cartElement = cartElementList.get(position)
+        holder.bindCartElement(cartElement)
+    }
+
+    class CartHolder(v: View, private val listener: PositionedClickListener) : RecyclerView.ViewHolder(v), View.OnClickListener {
+
+        private var view: View = v
+        private var removeProductButton : Button
+        private var removeOneItemButton : Button
+        private var addOneItemButton : Button
+
+        private var cartElement:CartElement? = null
+
+        private var bookListItemCover : ImageView
+        private var cartItemTitle : TextView
+        private var numberOfItems : TextView
+        private var itemPrice : TextView
+
+
+
+        init {
+            removeProductButton = view.findViewById(R.id.removeProductButton)
+            removeOneItemButton = view.findViewById(R.id.removeOneItemButton)
+            addOneItemButton = view.findViewById(R.id.addOneItemButton)
+
+            bookListItemCover = view.findViewById<ImageView>(R.id.bookListItemCover)
+            cartItemTitle = view.findViewById<TextView>(R.id.cartItemTitle)
+            numberOfItems = view.findViewById<TextView>(R.id.numberOfItems)
+            itemPrice = view.findViewById<TextView>(R.id.itemPrice)
+
+
+        }
+
+        override fun onClick(v: View?) {
+            v?: return
+
+            listener.onClick(v, adapterPosition)
+        }
+
+
+        fun bindCartElement(cartElement : CartElement){
+            this.cartElement = cartElement
+
+            //Récupérer détail d'un livre via le bookrepository
+
+            //Alimenter notre vue
+
+        }
+
+    }
+
+
+
+
 }
--- app/src/main/java/net/imt/fmsbookstore/ui/cart/CartFragment.kt
+++ app/src/main/java/net/imt/fmsbookstore/ui/cart/CartFragment.kt
@@ -5,16 +5,44 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import net.imt.fmsbookstore.R
+import net.imt.fmsbookstore.data.cart.CartElement
+import net.imt.fmsbookstore.ui.PositionedClickListener
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import timber.log.Timber
 
 class CartFragment: Fragment() {
     private val viewModel: CartViewModel by viewModel()
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var cartAdapter: CartAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val view = inflater.inflate(R.layout.cart, container, false)
+        linearLayoutManager = LinearLayoutManager(this.context)
+        recyclerView = view.findViewById(R.id.cartRecyclerView)
+        recyclerView.layoutManager = linearLayoutManager
+
+        val cartElements = ArrayList<CartElement>()
+        cartAdapter = CartAdapter(cartElements, object: PositionedClickListener{
+            override fun onClick(v: View, position: Int) {
+                val cartElement = cartAdapter.cartElementList
+
+                if(v.id == R.id.removeProductButton){
+                    Timber.i("Test")
+                }
+            }
+
+        })
+
+
         return super.onCreateView(inflater, container, savedInstanceState)
     }
 
