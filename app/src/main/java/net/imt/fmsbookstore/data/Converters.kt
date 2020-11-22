3fc76256490418370b73ae74822384bb26535a43
Marc LAMY <marclamy.bzh@gmail.com>
2020-11-22 20:53:29 +0100

    Added offers converters

--- app/src/main/java/net/imt/fmsbookstore/data/Converters.kt
+++ app/src/main/java/net/imt/fmsbookstore/data/Converters.kt
@@ -1,6 +1,8 @@
 package net.imt.fmsbookstore.data
 
 import androidx.room.TypeConverter
+import net.imt.fmsbookstore.data.cart.CommercialOffer
+import net.imt.fmsbookstore.data.cart.Offer
 
 object Converters {
     @TypeConverter
@@ -13,5 +15,26 @@ object Converters {
     @JvmStatic
     fun stringToList(str: String): List<String>{
         return str.split("\n")
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun offerToString(offer: Offer): String{
+        return offer.type + "|" + offer.value + "|" + offer.sliceValue?: ""
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun stringToOffer(str: String): Offer{
+        val offerParts = str.split("|")
+        val type = offerParts[0]
+        val value = offerParts[1].toInt()
+
+        if (offerParts[2].isEmpty()) {
+            return Offer(type, value, null)
+        }
+        else {
+            return Offer(type, value, offerParts[2].toInt())
+        }
     }
 }
